d9eabedcccf3ee506ed87c0509860de5e2ef94e0
Charvi Upreti <charviupreti@gmail.com>
2023-10-27 11:08:06 +0530

    first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+function Nav() {
+  return (
+    <nav class="navbar bg-warning">
+      <Link style={{ fontFamily: "Rajdhani" }} to="/" class="navbar-brand mx-3">
+        CRUD operation
+      </Link>
+      <div class="nav">
+        <Link
+          style={{ fontFamily: "BlackOpsOne" }}
+          to="/create-student"
+          class="nav-link"
+        >
+          Create Student
+        </Link>
+        <Link
+          style={{ fontFamily: "BlackOpsOne" }}
+          to="/student-list"
+          class="nav-link"
+        >
+          Student List
+        </Link>
+      </div>
+    </nav>
+  );
+}
+export default Nav;
